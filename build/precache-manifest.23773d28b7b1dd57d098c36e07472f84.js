9b36a71fa24183fc070f09c7d2efc7d9a5787dd6
bansalharshit95 <bansalharshit95@gmail.com>
2020-08-05 18:37:52 +0530

    pushing build

---
+++ build/precache-manifest.23773d28b7b1dd57d098c36e07472f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c136683a1d9d3c406224ec7d8dbe5c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35093a2debb428842b2c",
+    "url": "/static/css/main.7c04019f.chunk.css"
+  },
+  {
+    "revision": "78b94d6c3ee687815334",
+    "url": "/static/js/2.ad37782d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ad37782d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35093a2debb428842b2c",
+    "url": "/static/js/main.7338bd22.chunk.js"
+  },
+  {
+    "revision": "d8f073e9e201ff177010",
+    "url": "/static/js/runtime-main.56e437ee.js"
+  }
+]);
